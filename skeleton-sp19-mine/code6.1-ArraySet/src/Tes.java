b21b6380d91b2b23333bd7e89cde79d9993f2f6b
sunmiao0301 <hfut_sm@163.com>
2022-02-21 10:38:32 +0800

    3

--- skeleton-sp19-mine/code6.1-ArraySet/src/Tes.java
+++ skeleton-sp19-mine/code6.1-ArraySet/src/Tes.java
@@ -1,7 +1,16 @@
 public class Tes {
     public static void main(String[] args){
-        int[] a = new int[1];
-        int[] b = new int[]{}
-        System.out.println((0-1)/2);
+        int[] a = new int[]{1};
+        int[] b = new int[]{2};
+
+        int i = 1;
+        double d = 2.0;
+
+        System.out.println(add(d, d));
+        //System.out.println(add(i, d));
     }
+    public static double add(int i, double d){
+        return i + d;
+    }
+    
 }
